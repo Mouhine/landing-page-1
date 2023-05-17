d074646c55eade9ecb8c7ae8843bae2466742c0b
mohammed mouhine <mouhinemohmade@gmail.com>
2023-05-17 13:28:58 +0100

    fix hero

--- src/components/heroSection/HeroInfo.tsx
+++ src/components/heroSection/HeroInfo.tsx
@@ -3,7 +3,7 @@ const HeroInfo = () => {
     <div className="flex items-center justify-center py-6 ">
       <section className="md:ml-[200px]  flex items-start flex-col space-y-4  ">
         <span className="slug">100% Natural Food</span>
-        <h1 className="text-4xl text-[#274C5B] font-extrabold ">
+        <h1 className="text-4xl text-[#274C5B] font-extrabold md:w-[80%] ">
           Choose the best healthier way of life
         </h1>
         <button className="text-[10px]  bg-yellow-500 rounded-lg p-3 text-[#274C5B] ">
